fe65e246bf148ecc30ab0f5e7ed72b81d8ccd5ec
jordisanchoitb <jordi.sancho.7e7@itb.cat>
2024-04-03 00:50:03 +0200

    constantes a todos los mensajes

--- Ac1_IntergalacticExplorationMissions/Program.cs
+++ Ac1_IntergalacticExplorationMissions/Program.cs
@@ -6,6 +6,21 @@ namespace Ac1_IntergalacticExplorationMissions
     {
         public static void Main()
         {
+            const string WelcomeMessage = "Intergalactic Exploration Missions";
+            const string SeparatorWelcome = "===================================";
+            const string NumScore = "Score {0}";
+            const string MsgPlayer = "Introdueix el nom del jugador (només pot contenir caràcters alfabètics, sense accents ni caràcters especials)";
+            const string MsgErrorPlayer = "Nom incorrecte. Torna a introduir el nom del jugador (només pot contenir caràcters alfabètics, sense accents ni caràcters especials)";
+            const string MsgMission = "Introdueix el nom de la missio (ha de contenir com a prefix el nom adaptat de les consonants en grec, seguit d’un guió i un número de 3 xifres.\nExemple: Delta-003)";
+            const string MsgErrorMission = "Nom de missió incorrecte. Torna a introduir el nom de la missió (ha de contenir com a prefix el nom adaptat de les consonants en grec, seguit d’un guió i un número de 3 xifres.\nExemple: Delta-003)";
+            const string MsgScoring = "Introdueix la puntuació (un número enter entre 0 i 500 inclosos)";
+            const string MsgErrorScoring = "Puntuació incorrecta. Torna a introduir la puntuació (un número enter entre 0 i 500 inclosos)";
+            const string InputPlayer = "Player: ";
+            const string InputMission = "Mission: ";
+            const string InputScoring = "Scoring: ";
+            const string MsgRanking = "Rànquing de puntuacions úniques";
+            const string SeparatorRanking = "===============================";
+
             const int MAX_SCORES = 5;
             List<Score> scores = new List<Score>();
             string player;
@@ -13,45 +28,45 @@ namespace Ac1_IntergalacticExplorationMissions
             int scoring;
             bool error = false;
 
-            Console.WriteLine("Intergalactic Exploration Missions");
-            Console.WriteLine("===================================");
+            Console.WriteLine(WelcomeMessage);
+            Console.WriteLine(SeparatorWelcome);
             Console.WriteLine();
 
             for (int i = 0; i < MAX_SCORES; i++)
             {
-                Console.WriteLine($"Score {i+1}");
-                Console.WriteLine("Introdueix el nom del jugador (només pot contenir caràcters alfabètics, sense accents ni caràcters especials)");
+                Console.WriteLine(NumScore,i+1);
+                Console.WriteLine(MsgPlayer);
                 do
                 {
                     if (error)
                     {
-                        Console.WriteLine("Nom incorrecte. Torna a introduir el nom del jugador (només pot contenir caràcters alfabètics, sense accents ni caràcters especials)");
+                        Console.WriteLine(MsgErrorPlayer);
                     }
-                    Console.Write("Player: ");
+                    Console.Write(InputPlayer);
                     player = Console.ReadLine().Trim();
                     error = true;
                 } while (!Methods.CheckPlayer(player));
                 error = false;
-                Console.WriteLine("Introdueix el nom de la missio (ha de contenir com a prefix el nom adaptat de les consonants en grec, seguit d’un guió i un número de 3 xifres.\nExemple: Delta-003)");
+                Console.WriteLine(MsgMission);
                 do
                 {
                     if (error)
                     {
-                        Console.WriteLine("Nom de missió incorrecte. Torna a introduir el nom de la missió (ha de contenir com a prefix el nom adaptat de les consonants en grec, seguit d’un guió i un número de 3 xifres.\nExemple: Delta-003)");
+                        Console.WriteLine(MsgErrorMission);
                     }
-                    Console.Write("Mission: ");
+                    Console.Write(InputMission);
                     mission = Console.ReadLine().Trim();
                     error = true;
                 } while (!Methods.CheckMission(mission));
                 error = false;
-                Console.WriteLine("Introdueix la puntuació (un número enter entre 0 i 500 inclosos)");
+                Console.WriteLine(MsgScoring);
                 do
                 {
                     if (error)
                     {
-                        Console.WriteLine("Puntuació incorrecta. Torna a introduir la puntuació (un número enter entre 0 i 500 inclosos)");
+                        Console.WriteLine(MsgErrorScoring);
                     }
-                    Console.Write("Scoring: ");
+                    Console.Write(InputScoring);
                     scoring = Convert.ToInt32(Console.ReadLine());
                     error = true;
                 } while (!Methods.CheckScoring(scoring));
@@ -62,8 +77,8 @@ namespace Ac1_IntergalacticExplorationMissions
 
             List<Score> ranking = Methods.GenerateUniqueRanking(scores);
             ranking.Sort();
-            Console.WriteLine("Rànquing de puntuacions úniques");
-            Console.WriteLine("===============================");
+            Console.WriteLine(MsgRanking);
+            Console.WriteLine(SeparatorRanking);
             foreach (Score score in ranking)
             {
                 Console.WriteLine($"{score.Player} - {score.Mission} - {score.Scoring}");
