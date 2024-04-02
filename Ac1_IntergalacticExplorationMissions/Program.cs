c2407ab205e24064f512471e497de032ca917b41
jordisanchoitb <jordi.sancho.7e7@itb.cat>
2024-04-02 13:00:35 +0200

    initial commit

---
+++ Ac1_IntergalacticExplorationMissions/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ac1_IntergalacticExplorationMissions
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            
+        }
+    }
+}
